68aa54081d994173e94a67ea5dceddee24b79821
Khaled El Hamzi <contact@elhamzi.me>
2021-05-22 02:08:20 +0200

    Handle error refactor deprecated method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PostService } from '../services/post.service';
 
 @Component({
@@ -8,6 +9,7 @@ import { PostService } from '../services/post.service';
 })
 export class PostsComponent implements OnInit {
   posts: Array<any> = [];
+  postSubscription: Subscription = new Subscription;
 
   constructor(private service: PostService) {
   }
@@ -18,19 +20,21 @@ export class PostsComponent implements OnInit {
 
     this.service
         .createPost(post)
-        .subscribe(response => {
+      .subscribe({
+        next: response => {
         post.id = (response as any).id;
         // this.posts.unshift(post);
         this.posts.splice(0, 0, post);
-        }, (error: Response) => {
+        },
+        error: (error: Response) => {
           if (error.status === 400) {
             // this.form.setErrors(error.json())
           }
           else{
-           alert('An unexpected error occurred.');
+            console.error('An unexpected error occurred.', error);
             console.log(error);
           }
-        });
+        }});
   }
 
   updatePost(post: any): void {
@@ -38,39 +42,72 @@ export class PostsComponent implements OnInit {
     // this.http.put(this.url, JSON.stringify(post))
     this.service
         .updatePost(post)
-        .subscribe(response => {
-        console.log(response);
-      }, (error: any) => {
-          alert('An unexpected error occurred.');
-          console.log(error);
-        });
+      .subscribe({
+         next: (response) => {
+          console.log(response);
+      },
+      error: (error: any) => {
+        console.error('An unexpected error occurred.', error);
+        console.log(error);
+      }});
   }
 
   deletePost(post: any): void {
+    // this.service
+    //     .deletePost(post.id)
+    //     .subscribe(response => {
+    //     const index = this.posts.indexOf(post);
+    //     this.posts.splice(index, 1);
+    //     }, (error: Response) => {
+    //       if (error.status === 404)
+    //         alert('This post has already been deleted.')
+    //       else
+    //         alert('An unexpected error occurred.');
+    //         console.log(error);
+    //     });
+    // the above method is deprecated
+
     this.service
         .deletePost(post.id)
-        .subscribe(response => {
-        const index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-        }, (error: Response) => {
+      .subscribe({
+        next: (response) => {
+          const index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        },
+        error:  (error: Response) => {
           if (error.status === 404)
-            alert('This post has already been deleted.')
+          console.log('This post has already been deleted.');
           else
-            alert('An unexpected error occurred.');
+            console.error('An unexpected error occurred.', error),
             console.log(error);
-        });
+        }});
+  }
+
+  getPosts() {
+    this.postSubscription = this.service
+    .getPosts()
+    .subscribe({
+      next: (response) => this.posts = response as Array<any>,
+      error: (error:Response) => console.error('An unexpected error occurred.', error),
+      complete: () => console.info('complete')
+    })
   }
 
   ngOnInit(): void {
-    this.service
-        .getPosts()
-      .subscribe(
-          response => {
-          this.posts = response as Array<any>;
-        },
-          error  => {
-          alert('An unexpected error occurred.');
-          console.log(error);
-        });
+    // this.service
+    //     .getPosts()
+    //   .subscribe(
+    //       response => {
+    //       this.posts = response as Array<any>;
+    //     },
+    //       error  => {
+    //       alert('An unexpected error occurred.');
+    //       console.log(error);
+    //     });
+    this.getPosts();
+  }
+
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
   }
 }
