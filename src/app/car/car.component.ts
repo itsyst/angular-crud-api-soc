ab0c0ded001e9149a6aa7431ba671ee43815c413
Khaled El Hamzi <contact@elhamzi.me>
2021-05-26 12:55:46 +0200

    Create car component

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CarService } from '../services/car.service';
+import { Car } from '../types/Car';
+
+@Component({
+  selector: 'car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+  cars: Car[] = [];
+
+  constructor(private carService: CarService) { }
+
+  getCars(): void {
+    this.carService
+    .getAll()
+      .subscribe((cars: Car[]) => {
+        this.cars = Object.values(Object.values(cars)[0]);
+      })
+  }
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+
+}
