ac873e1df6c3e40df79142c3d2a894d5096f320d
Khaled El Hamzi <contact@elhamzi.me>
2021-05-26 12:57:50 +0200

    Add fake api to retrieve car data

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   title: 'Dev Environment Heading',
   apiURL: 'http://jsonplaceholder.typicode.com/posts/',
-  apiGitHub: 'https://api.github.com/users/itsyst/followers'
+  apiGitHub: 'https://api.github.com/users/itsyst/followers',
+  apiCar:'https://myfakeapi.com/api/cars'
 };
