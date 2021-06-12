c22bab7867367fc790cf71656e6488b519ccabbe
Khaled El Hamzi <contact@elhamzi.me>
2021-06-12 20:36:23 +0200

    Playing around with test env

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  title: 'Prod Environment Heading',
+  navBarBackgroundColor: 'red',
+  apiURL: 'http://jsonplaceholder.typicode.com/posts/',
+  apiGitHub: 'https://api.github.com/users/itsyst/followers',
+  apiCar:'https://myfakeapi.com/api/cars'
+};
